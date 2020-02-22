9440bf485f97e81988d1030fc0d76f57be19f3bc
zzpapy <gregory.pace@hotmail.fr>
2020-02-22 16:26:53 +0100

    yahtzee bonus

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let nbLancer = 0
 let nbParties = 0
-
+let finJeu = 0
+let grandTotal = 0
 function randomDiceValue(){
 
     let nbAlea = Math.floor(((Math.random()*10)%6)+1)
     
     return nbAlea
 }
+
 $(".parties").html('Nombre de parties jouées : '+nbParties)
 function endTurn(){
     $("#roll").hide()
@@ -23,10 +25,9 @@ $(document).ready(function(){
     $("#plateau .dice").css("visibility", "hidden")
     $("#makeScore").hide()
 });
-
 $("#roll").click(function(){
-    nbLancer++
     $(this).data("nbrLancer",nbLancer)
+    nbLancer++
     $("#plateau .dice").css("visibility", "visible")
     
     $(".dice:not(.blocked) img").each(function(){
@@ -42,14 +43,50 @@ $("#roll").click(function(){
     
     if(nbLancer == 3){
         nbParties++
+        
         $(".parties").html('Nombre de parties jouées : '+nbParties)
-        console.log("toto")
         $(".raye").each(function(){
-            $(this).append("toto")
+            $(this).toggleClass("choix")
+            $(this).children(".choix_score + td").show()
+            $(this).children(".choix_score + td").html("<img style ='width:3em;height:3em' src = 'img/cross.png'>")
+            if(finJeu == 2){
+                $("#plateau").remove()
+                $("#score").remove()
+                $(".fini").html("<div>C'EST FINI</div>")
+                $(".fini").append(grandTotal)
+                console.log(grandTotal)
+                // $(".fin").html(grandTotal)
+            }
         })
+        finJeu++
         endTurn()
     }
 })
+$(".choix2").click(function(){
+    $(this).closest("tr").remove()
+        $(".choix").each(function(){
+            $(".raye.choix").each(function(){
+                $(this).toggleClass("choix")
+                $(this).children(".choix_score + td").html("")
+                $(this).children(".choix_score + td").hide()
+            })
+        })
+        
+        $(".dice.blocked").toggleClass("blocked")
+        nbLancer++
+    $(".dice:not(.blocked) img").each(function(){
+    
+        let nbAlea = randomDiceValue()
+        
+        let pathImg = "./img/inverted-dice-"+nbAlea+".png";
+        
+        $(this).attr("src", pathImg)
+        $(this).data("nb", nbAlea)
+        
+    })
+    startTurn();
+})
+
 
 $(".dice").click(function(){
     $(this).toggleClass("blocked")
@@ -78,12 +115,12 @@ $("#makeScore").click(function(){
 //     nbParties++
    
 // })
-function myFunc(arr){
-    var x= arr[0];
-    return arr.every(function(item){
-        return item=== x;
-    });
-}
+// function myFunc(arr){
+//     var x= arr[0];
+//     return arr.every(function(item){
+//         return item=== x;
+//     });
+// }
 $(".choix_score").click(function(){
     tab=[]
     
@@ -94,6 +131,8 @@ $(".choix_score").click(function(){
         
     })
     const allEqual = arr => arr.every( v => v === arr[0] )
+    
+    console.log(tab);
     if(allEqual(tab )){
         if(tab[0]==$(this).data("value") && $.isNumeric($(this).data("value"))){
             if($(this).html() == "0"){
@@ -107,10 +146,8 @@ $(".choix_score").click(function(){
             }
         }
         else if(allEqual(tab ) && !($.isNumeric($(this).data("value")))){
-            console.log(tab.length ==4 , $(this).data("value") == "carre",somme);
             switch(true) {
                 case tab.length ==3 && $(this).data("value") == "brelan":
-                    console.log('toto')
                     $(this).empty()
                     $(this).append(somme)
                     startTurn()
@@ -120,7 +157,6 @@ $(".choix_score").click(function(){
                     $(".dice.blocked").toggleClass("blocked")
                     break;
                 case tab.length == 4 && $(this).data("value") == "carre":
-                    console.log('toto')
                     $(this).empty()
                     $(this).append(somme)
                     startTurn()
@@ -130,6 +166,11 @@ $(".choix_score").click(function(){
                     $(".dice.blocked").toggleClass("blocked")
                     break;
                 case tab.length == 5 && $(this).data("value") == "YAHTZ":
+                    if($(this).html() !="0"){
+                        console.log($(this).html())
+                        $(".bonus").html(parseInt($(".bonus").html()) + 100)
+                         
+                    }
                     console.log('toto')
                     $(this).empty()
                     $(this).append("50")
@@ -244,9 +285,9 @@ $(".choix_score").click(function(){
             
         }
     }
+    console.log(grandTotal)
     let total = 0
     let total1 = 0
-    grandTotal = 0
     $(".upper").each(function(){
         total+= parseInt($(this).html())
         
@@ -256,7 +297,54 @@ $(".choix_score").click(function(){
         total1+= parseInt($(this).html())
     })
     $(".total_lower").html(total1)
-    grandTotal+= total + total1
+    grandTotal = total + total1
     $(".grand_total").html(grandTotal)
-    console.log(grandTotal)
+    console.log(grandTotal,total,total1,nbParties)
+    
+    $(".choix").each(function(){
+        $(".raye.choix").each(function(){
+            $(this).toggleClass("choix")
+            $(this).children(".choix_score + td").html("")
+        })
+    })
+    nbLancer++
+    $(".dice:not(.blocked) img").each(function(){
+    
+        let nbAlea = randomDiceValue()
+        
+        let pathImg = "./img/inverted-dice-"+nbAlea+".png";
+        
+        $(this).attr("src", pathImg)
+        $(this).data("nb", nbAlea)
+        
+    })
+    if(finJeu == 2){
+        $("#plateau").remove()
+        $(".flex").remove()
+        $(".fini").html("<div>C'EST FINI</div>")
+        $(".fini").append(grandTotal)
+        console.log(grandTotal)
+    }
+    
+   
 })
+var face=1;
+var show = function() {
+    $('.cube1').attr('class', 'cube cube1 show'+face);
+        
+    let nbAlea = randomDiceValue()
+
+    face=nbAlea
+};
+
+var show2 = function() {
+  $('.cube2').attr('class', 'cube cube2 show'+face);
+  if(face==6) {
+    face=1;
+  } else {
+    face++; 
+  }
+};
+
+$('.cube1').on('click', show);
+$('.cube2').on('click', show2);
